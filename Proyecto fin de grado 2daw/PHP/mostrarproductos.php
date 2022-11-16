9a1478526633d4021fbc824cefd28dce80334735
guillermo lopez <91660695+guillermo2512@users.noreply.github.com>
2022-11-16 19:48:14 +0100

    cosasnuevas16/11

---
+++ Proyecto fin de grado 2daw/PHP/mostrarproductos.php
@@ -0,0 +1,41 @@
+<?php
+
+include_once "conexion.php";
+
+class Comprar {
+
+    private $conexion;
+
+    public function getconecBD()
+    {
+        $dbObj = new conectaBD();
+		$this->conexion = $dbObj->db;
+    }
+
+
+    public function enviar_ajax()
+    {
+        $this->getconecBD();
+        try {
+            $consulta = $this->conexion->prepare("SELECT ID_Articulo, Titulo, Precio FROM productos;");
+
+            if (!$consulta->execute()) {
+                print_r($consulta->errorInfo());
+                return false;
+            }
+
+            $almacen = $consulta->fetchAll();
+            $envio = json_encode($almacen);
+            echo $envio;
+
+        } catch (PDOException $e) {
+            die("Error: " . $e->getMessage());
+        }
+    }
+}
+
+$condb = new Comprar();
+$condb -> enviar_ajax();
+
+
+?>
