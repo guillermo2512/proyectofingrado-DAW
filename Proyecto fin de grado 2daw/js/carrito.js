d37fb7b997bbb1667287d6c1ea1f2f3ad062540a
guillermo <guillermo.lmm@gmail.com>
2022-11-25 18:00:01 +0100

    cambios js y html 25/11

--- Proyecto fin de grado 2daw/js/carrito.js
+++ Proyecto fin de grado 2daw/js/carrito.js
@@ -1,30 +1,33 @@
 
+var articulos = [];
 var articulo = {
     id: 0
 }
 mostrarindice();
 
+if (localStorage.getItem("articulos") != null) {
+    var articulos = JSON.parse(localStorage.getItem('articulos'));
+}
+
 var Buttons = document.querySelectorAll('button.boton');
 
 Buttons.forEach(function (item) 
 {
-    item.addEventListener('click', function (e) 
+    item.addEventListener('click', function () 
     { 
-        e.preventDefault();
         const id = parseInt(item.dataset.id);
         const precio = item.dataset.precio;
         const titulo = item.dataset.titulo;
-
-        let articulos = [];
         
         articulo = {
             id: id,
             precio: precio,
             titulo: titulo,
-            //cantidad: 1
+            cantidad: 1
         }
         articulos.push(articulo);
 
+        //si se repite borrar el anterior con con el valor menos 1
         /*if (articulos.length == 0) 
         { 
             articulo = {
@@ -37,6 +40,44 @@ Buttons.forEach(function (item)
 
         }else{
 
+            const lookup = articulos.reduce((a, e) => {
+                a[e.id] = ++a[e.id] || 0;
+                return a;
+              }, {});
+              
+              const datos = articulos.filter(e => lookup[e.id])
+              
+              // Se reduce el arreglo data2 para formar un indice
+              const index = datos.reduce((prev, curr) => {
+                  return {
+                      ...prev,
+                      [curr.id]: curr.cantidad
+                  }
+              },
+              {})
+              
+              console.log(index);
+              
+              // Se reduce el arreglo data1 comparandolo respecto al indice
+              const res = values.reduce((prev, curr) => {
+                  // Si las cantidades son iguales se agrega el objeto a los correctos
+                  if (index[curr.id] !== curr.cantidad) {
+                      return {
+                          right: [...prev.right, curr],
+                          wrong: prev.wrong
+                      }
+                  }
+                  // De lo contrario se agrega a los incorrectos
+                  return {
+                      right: prev.right,
+                      wrong: [...prev.wrong, curr]
+                  }
+              },
+              {
+                  right: [],
+                  wrong: []
+              })
+            
             articulos.forEach(element => {
                 var ids = element.id;
                 if(ids!= id)
@@ -54,8 +95,9 @@ Buttons.forEach(function (item)
                     articulo.cantidad = alamcen + 1;
                 }
             });
-            
         }*/
+
+        
         
         localStorage.setItem('articulos', JSON.stringify(articulos));
         mostrarindice();
@@ -72,5 +114,4 @@ function mostrarindice(){
         }
     }
 }
-
 
